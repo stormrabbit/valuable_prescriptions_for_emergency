d21889a174702395d2356abb246d92debb9b48a8
龙骑 <catwindboy@hotmail.com>
2021-05-14 18:32:00 +0800

    feature: 链式表达式未完成

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/babel-preset-jsx'
+  ],
+  'plugins': [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ],
+    ["@babel/plugin-syntax-optional-chaining"]
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './plugins/router'
+import api from './plugins/axios'
+Vue.prototype.$api = api
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
